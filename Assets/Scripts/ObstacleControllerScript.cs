2c0a4805b28475a82d0fbdd0d9849a2ec2786aa5
anisimovics <eriks.anisimovics@sk.lvt.lv>
2025-11-06 11:25:45 +0200

    Fixed object fade

--- Assets/Scripts/ObstacleControllerScript.cs
+++ Assets/Scripts/ObstacleControllerScript.cs
@@ -44,14 +44,14 @@ public class ObstacleControllerScript : MonoBehaviour
         rectTransform.anchoredPosition += new Vector2(-speed * Time.deltaTime, waveOffset * Time.deltaTime);
 
         // Iznīcinās ja lido pa kreisi
-        if (speed > 0 && transform.position.x < (screenBoundriesScript.minX + 80) && !isFadingOut)
+        if (speed > 0 && transform.position.x < (screenBoundriesScript.worldBounds.min.x + 80) && !isFadingOut)
         {
             isFadingOut = true;
             StartCoroutine(FadeOutAndDestroy());
         }
 
         // Iznīcinās ja lido pa labi
-        if (speed < 0 && transform.position.x > (screenBoundriesScript.maxX - 80) && !isFadingOut)
+        if (speed < 0 && transform.position.x > (screenBoundriesScript.worldBounds.max.x - 80) && !isFadingOut)
         {
             isFadingOut = true;
             StartCoroutine(FadeOutAndDestroy());
